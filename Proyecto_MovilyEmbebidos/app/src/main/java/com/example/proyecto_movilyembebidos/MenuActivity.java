c9e28cb709325aab74a906d5d9ee770ee578a34c
JacielH <111033129+JacielH@users.noreply.github.com>
2022-12-27 17:51:47 -0600

    Contactos terminados

--- Proyecto_MovilyEmbebidos/app/src/main/java/com/example/proyecto_movilyembebidos/MenuActivity.java
+++ Proyecto_MovilyEmbebidos/app/src/main/java/com/example/proyecto_movilyembebidos/MenuActivity.java
@@ -13,25 +13,22 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MenuActivity extends AppCompatActivity {
     Button btnConta1, btnConta2, btnConta3, btnCont1, btnCont2, btnCont3;
-    private String ONContacto1, ONContacto2, ONContacto3, OFFContacto1, OFFContacto2, OFFContacto3;
     RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
-        //ONContacto1="http://192.168.100.53/relay?r1=on";
-        //OFFContacto1="http://192.168.100.53/relay?r1=off";
-        //ONContacto2="http://192.168.100.53/relay?r2=on";
-        //OFFContacto2="http://192.168.100.53/relay?r2=off";
-        //ONContacto3="http://192.168.100.53/relay?r3=on";
-        //OFFContacto3="http://192.168.100.53/relay?r3=off";
 
         btnConta1=(Button) findViewById(R.id.btnConta1);
         btnCont1=(Button) findViewById(R.id.btnCont1);
@@ -43,54 +40,61 @@ public class MenuActivity extends AppCompatActivity {
         btnConta1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                actualizar("http://192.168.100.53/");
+                Buscar("http://192.168.100.53/relay?r1=on");
             }
         });
         btnCont1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                actualizar("http://192.168.100.53/relay?r1=off");
+                Buscar("http://192.168.100.53/relay?r1=off");
             }
         });
         btnConta2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                actualizar("http://192.168.100.53/relay?r2=on");
+                Buscar("http://192.168.100.53/relay?r2=on");
             }
         });
         btnCont2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                actualizar("http://192.168.100.53/relay?r2=off");
+                Buscar("http://192.168.100.53/relay?r2=off");
             }
         });
         btnConta3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                actualizar("http://192.168.100.53/relay?r3=on");
+                Buscar("http://192.168.100.53/relay?r3=on");
             }
         });
         btnCont3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                actualizar("http://192.168.100.53/relay?r3=off");
+                Buscar("http://192.168.100.53/relay?r3=off");
             }
         });
     }
-    private void actualizar(String url){
-        StringRequest stringRequest= new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+    private void Buscar(String url) {
+        JsonArrayRequest stringArray = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(String response) {
-                Toast.makeText(getApplicationContext(), "Operacion realizada", Toast.LENGTH_LONG).show();
+            public void onResponse(JSONArray response) {
+                JSONObject jsonObject = null;
+                for (int i = 0; i <= response.length(); i++) {
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        jsonObject.getString("relay");
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(),error.toString(), Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Contacto Encendido", Toast.LENGTH_LONG).show();
             }
-        }){
-        };
-        RequestQueue requestQueue= Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringArray);
     }
 }
